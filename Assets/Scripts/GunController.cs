1513d1c92769dd0a92e7fe0522d05ada7b4ef081
Gary <fishellgary@gmail.com>
2020-04-26 16:31:12 -0700

    Added player machine gun script/models and created particle effects for muzzle flash

---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public float fireRate = 15f;
+    public float nextFire = 0f;
+    public Camera  playerCam;
+    public ParticleSystem muzzleFlash;
+    // Update is called once per frame
+    void Update()
+    {
+        //Fire weapon
+        if(Input.GetMouseButton(0) && Time.time >= nextFire)
+        {
+            nextFire = Time.time + 1f/fireRate;
+
+            muzzleFlash.Play();
+            RaycastHit hitInfo;
+            if(Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out hitInfo, range)) {
+                Debug.Log(hitInfo.transform.name);
+            }
+        }
+
+    }
+}
